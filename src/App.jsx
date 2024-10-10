93f34f4c3a46ee65a292b6f60c8b7fe35579b54a
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-10-11 00:09:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     <div className="random-quote-generator flex justify-center flex-col">
       <div className='quote-wrapper mt-8 m-6 p-4 w-fit h-fit border-2 border-amber-300 bg-stone-100 place-self-center text-slate-950 shadow-md shadow-slate-700 rounded-sm'>
         <p className='font-lexend font-bold'>Author : {quote?.author}</p>
-        <p className='font-ubuntu line-clamp-5'>Quote : {quote?.content}</p>
+        <p className='font-ubuntu line-clamp-5'><span className="font-bold font-ubuntu">Quote</span> : {quote?.content}</p>
       </div>
       <button className="px-4 py-2 rounded-md mt-6 w-fit h-fit bg-slate-950 text-gray-50 hover:bg-gray-400 hover:text-slate-950 place-self-center font-lexend shadow-md shadow-black" onClick={handleRefresh}>Generate Quote</button>
     </div>
